171812b121dcf1f99155b85be33a146eb6454d73
denis <denispolyarush1405@gmail.com>
2024-02-25 21:55:12 +0300

    Finished

---
+++ src/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const arrGroups = [
+  'FE2021-5',
+  'Fe2020-2',
+  'FD2021',
+  'fD2022-1',
+  'FE2020-3',
+  'FD2020/2',
+  'fe2021-3',
+  'FD2020$5',
+  'FE2008-52',
+  'Fm2008-23',
+  'FM2022-1',
+];
+  const correct = [];
+  const incorrect = [];
+arrGroups.forEach((item) => {
+  let reg = /^(FE|FD|FM|fe|fd|fm)(20\d{2})(-\d{0,2})?$/;
+    if (reg.test(item)) {
+      console.log(`Success ${item}`);
+      correct.push(`Success ${item}`);
+    return;
+    }
+    console.log(`Error ${item}`);
+  incorrect.push(`Error ${item}`)
+});
+
+console.log(correct)
+console.log(incorrect)
